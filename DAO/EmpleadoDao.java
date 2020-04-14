46759fc3935200db08ffff5882ed0de914d91bd6
Daniel <danielcaos@ufps.edu.co>
2020-04-14 16:27:56 -0500

    mas cosas

--- DAO/EmpleadoDao.java
+++ DAO/EmpleadoDao.java
@@ -2,26 +2,30 @@ import java.sql.SQLException;
 
 public class EmpleadoDao {
 
-	 Conexion con = null;
+	Conexion con = null;
 
-	    public EmpleadoDao() {
-	        con = Conexion.getConexion();
-	    }
+	public EmpleadoDao() {
+		con = Conexion.getConexion();
+	}
 
-	    public boolean registrar (Empleado nomina) {
-	        String sql = "INSERT INTO empleado (codigo, cedula, nombre, fechanacimiento, fechaingreso, fecharetiro)"
-	                + "VALUES ('"+nomina.getCodigo()+"','"+nomina.getCedula()+"','"+nomina.getNombre()+"','"+nomina.getFechaN()+"','"+nomina.getFechaI()+"','"+nomina.getFechaR()+"')";
-	        try {
-	            con.insert(sql);
-	            return true;
-	        } catch (SQLException e) {
-	            return false;
-	        }
-	        
-	    }
-	    
-	    public void buscar (int codigo) {
-	    	String sql = "SELECT codigo, cedula, nombre, fechanacimiento, fechaingreso, fecharetiro FROM empleado"+" where codigo ="+codigo;
-	    }
-	
+	public boolean registrar(Empleado nomina) {
+		String sql = "INSERT INTO empleado (codigo, cedula, nombre, fechanacimiento, fechaingreso, fecharetiro)"
+				+ "VALUES ('" + nomina.getCodigo() + "','" + nomina.getCedula() + "','" + nomina.getNombre() + "','"
+				+ nomina.getFechaN() + "','" + nomina.getFechaI() + "','" + nomina.getFechaR() + "')";
+		try {
+			con.insert(sql);
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+
+	}
+
+	public void buscar(int codigo) {
+		String sql = "SELECT codigo, cedula, nombre, fechanacimiento, fechaingreso, fecharetiro FROM empleado where codigo ="
+				+ codigo;
+
+		
+	}
+
 }
